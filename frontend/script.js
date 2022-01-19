1e05be7c43deaf572edaa285d085bf48285cba10
Kinga Kohari <kinguca@yahoo.com>
2022-01-19 17:41:22 +0100

    Data structures added

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,51 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favouriteFood: "csirke/rizs"
+    },
+    {
+        name: "András Varga",
+        favouriteFood: "brassói"
+    },
+    {
+        name: "Buczkó Balázs",
+        favouriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name: "Krisztián Bui",
+        favouriteFood: "banán"
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favouriteFood: "avokádó"
+    },
+    {
+        name: "Benett Viszokai",
+        favouriteFood: "alma"
+    }
+]
 function loadEvent(){
     console.log ("Page has loaded");
+    const root = document.querySelector("#root")
 
-    const list = document.querySelectorAll("section");
-
-    for (const elem of list) {
-     elem.insertAdjacentHTML("beforeend", `
-     <p>text</p>
-     `)  
-     //elem.querySelector("h1").outerHTML = "" 
+    for (const frontendStudent of data) {
+     
+    root.insertAdjacentHTML("beforeend", `
+        <section>
+            <h1>${frontendStudent.name}<h1>
+            <p>${frontendStudent.favouriteFood}</p>
+        </section>
+    `)    
     }
+
 }
 window.addEventListener("load", loadEvent);
