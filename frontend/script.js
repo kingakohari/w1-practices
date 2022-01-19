be5d60bc8f40e4ea263450ce017bf5082fe4b44d
Kinga Kohari <kinguca@yahoo.com>
2022-01-19 16:35:26 +0100

    Console.log added

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 function loadEvent(){
-    console.log ("Page has loaded")
+    console.log ("Page has loaded");
 
-    const list = document.querySelectorAll("section")
+    const list = document.querySelectorAll("section");
 
     for (const elem of list) {
      elem.insertAdjacentHTML("beforeend", `
@@ -10,4 +10,4 @@ function loadEvent(){
      //elem.querySelector("h1").outerHTML = "" 
     }
 }
-window.addEventListener("load", loadEvent)
+window.addEventListener("load", loadEvent);
