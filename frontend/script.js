0afdca2f40d5a0a7f7574edbaa1155cc06be6377
Kinga Kohari <kinguca@yahoo.com>
2022-01-19 15:57:39 +0100

    Miscellaneous changes

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+function loadEvent(){
+    console.log ("Page has loaded")
+
+    const list = document.querySelectorAll("section")
+
+    for (const elem of list) {
+     elem.insertAdjacentHTML("beforeend", `
+     <p>text</p>
+     `)  
+     elem.querySelector("h1").outerHTML = "" 
+    }
+}
+window.addEventListener("load", loadEvent)
