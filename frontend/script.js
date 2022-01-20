79cc839364630585060d2dd0382ed035af19cc7b
Kinga Kohari <kinguca@yahoo.com>
2022-01-20 18:41:40 +0100

    Added final touches

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,24 @@
-async function loadEvent(){
-    console.log ("Page has loaded");
+async function loadEvent() {
+    console.log("Az oldal betöltődött")
 
-        const root = document.querySelectorAll("#root");
+    const root = document.querySelector("#root")
 
-        const countryRes = await fetch("https://restcountries.com/v3.1/all");
-        const countryArr = await countryRes.json();
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
 
-        //console.log(countryArr[0].name.common)
+    //console.log(countryArr[0].name.common)
 
-        for (const country of countryArr) {
-            //console.log(country.name.common)
+    for (const country of countryArr) {
 
-        root.insertAdjacentHTML("beforeend", `
+       //console.log(country.name.common);
+
+       root.insertAdjacentHTML("beforeend", `
             <section>
                 <h1>${country.name.common}</h1>
                 <p>${country.region}</p>
             </section>
-            `);
-        }
+        `)
+        
     }
-window.addEventListener("load", loadEvent);
+}
+window.addEventListener("load", loadEvent)
